d72cbd51be24ef854621ff1a2961a11314d0d969
zhinum <princeonah23@gmail.com>
2026-05-05 13:57:20 +0100

    initialize a form validation site with css and js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./validation.css";
+import { Validation } from "./validation.js";
+
+const formElement = document.querySelector("#validation-form");
+
+const vali = new Validation(formElement);
